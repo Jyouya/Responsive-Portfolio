06d2a623f7600d5b2dc3dd7ea0caf5f525ab8140
Will West <willwest8xp@gmail.com>
2019-08-12 18:43:22 -0400

    started portfolio page refactor

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,12 +5,13 @@ $(window).ready(function () {
 });
 
 const pageTable = {
-    about: drawAbout
+    about: drawAbout,
+    profile: drawProfile,
 }
 function changePage(newPage) {
     $('.nav-button').removeClass('current-page');
     $(this).addClass('current-page');
-    $('.content-box').animate({ left: '-100%', opacity: 0 }, 500).promise()
+    $('.content-box').animate({ opacity: 0, right: '110%' }, 500).promise()
         .then(
             function () {
                 $(this).remove();
@@ -20,7 +21,7 @@ function changePage(newPage) {
         .addClass('content-box start-transparent start-right')
         .appendTo('.content')
         .append(...pageTable[newPage]())
-        .animate({ opacity: 1, left: 0 }, 500).promise()
+        .animate({ opacity: 1, right: 0 }, 500).promise()
         .then(
             $(this).removeClass('start-right start-transparent')
         );
@@ -40,4 +41,55 @@ function drawAbout() {
                     currently focused window, a program to generate functional chord progressions, and a set of addons
                     for Final Fantasy 11.</p>
             </article>`)]
+}
+
+
+const portfolio = [
+    {
+        title: 'Animingle',
+        img: './assets/images/animingle.png',
+        link: 'https://zscott92.github.io/Animingle/',
+        source: 'https://github.com/zscott92/Animingle'
+    }
+]
+function drawProfile() {
+    return [$('<h2 class="page-title">Portfolio</h2>'),
+    $('<div class="portfolio">').append(
+        ...portfolio.map(entry => {
+            return $(`<a href="${entry.link}" class="thumbnail">${entry.source ? `
+            <i class="fab fa-github"></i>` : `
+            `}
+                        <label>${entry.title}</label>
+                        <img src="${entry.img}" alt="${entry.title}"/>
+                    </a>`)
+        })
+    )];
+
+    // $(`<div class="portfolio">
+    //     <a href="https://jyouya.github.io/Word-Guess-Game/" class="thumbnail">
+    //         <label>Hangman</label>
+    //         <img src="http://lorempixel.com/400/300/cats/1" alt="" />
+    //     </a>
+
+    //     <a href="https://jyouya.github.io/unit-4-game/" class="thumbnail">
+    //         <label>RPG Game</label>
+    //         <img src="http://lorempixel.com/400/300/cats/6" alt="" />
+    //     </a>
+
+    //     <a href="https://jyouya.github.io/TriviaGame/" class="thumbnail">
+    //         <label>Trivia Game</label>
+    //         <img src="http://lorempixel.com/400/300/cats/3" alt="" />
+    //     </a>
+
+    //     <a href="#" class="thumbnail">
+    //         <label>Rutgers Info Widget</label>
+    //         <img src="http://lorempixel.com/400/300/cats/4" alt="" />
+    //     </a>
+
+    //     <a href="#" class="thumbnail">
+    //         <label>Rock Paper Scissors</label>
+    //         <img src="http://lorempixel.com/400/300/cats/5" alt="" />
+    //     </a>
+
+    // </div>`)];
 }
