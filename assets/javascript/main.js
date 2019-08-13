1452aab27ffabc4be28f636318d211bd1db0f294
Will <willwest8xp@gmail.com>
2019-08-12 23:45:05 -0400

    more polish, completed portfolio and temporary contact page

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,6 +7,7 @@ $(window).ready(function () {
 const pageTable = {
     about: drawAbout,
     profile: drawProfile,
+    contact: drawContact,
 }
 function changePage(newPage) {
     $('.nav-button').removeClass('current-page');
@@ -40,7 +41,13 @@ function drawAbout() {
                     are: a program that sets your desktop background to a space-themed wallpaper that color-matches the
                     currently focused window, a program to generate functional chord progressions, and a set of addons
                     for Final Fantasy 11.</p>
-            </article>`)]
+            </article>
+            <br/>`),
+    $('<div class="links">').append(
+        $('<a href="mailto:willwest8xp@gmail.com"><i class="far fa-envelope"></i></a>'),
+        $('<a href="https://github.com/Jyouya"><i class="fab fa-github"></i></a>'),
+        $('<a href="https://www.linkedin.com/in/will-west-ab19ab97/"><i class="fab fa-linkedin-in"></i></a>')
+    )]
 }
 
 
@@ -49,47 +56,78 @@ const portfolio = [
         title: 'Animingle',
         img: './assets/images/animingle.png',
         link: 'https://zscott92.github.io/Animingle/',
-        source: 'https://github.com/zscott92/Animingle'
+        source: 'https://github.com/zscott92/Animingle',
+        label: false,
+    },
+    {
+        title: 'Windower4 GUI-Lib',
+        img: './assets/images/gui-lib.png',
+        link: 'https://github.com/Jyouya/GUI-lib',
+        label: true
+    },
+    {
+        title: 'Hangman',
+        img: './assets/images/hangman.png',
+        link: 'https://jyouya.github.io/Word-Guess-Game/',
+        source: 'https://github.com/Jyouya/Word-Guess-Game',
+        label: false,
+        lightIcon: true,
+    },
+    {
+        title: 'Anime Trivia',
+        img: './assets/images/trivia.png',
+        link: 'https://jyouya.github.io/TriviaGame/',
+        source: 'https://github.com/Jyouya/TriviaGame',
+        label: false,
+    },
+    {
+        title: 'Star Wars Puzzle RPG',
+        img: './assets/images/star-wars.png',
+        link: 'https://jyouya.github.io/unit-4-game/',
+        source: 'https://github.com/Jyouya/unit-4-game',
+        label: true,
+        lightIcon: true,
     }
 ]
+
 function drawProfile() {
     return [$('<h2 class="page-title">Portfolio</h2>'),
     $('<div class="portfolio">').append(
         ...portfolio.map(entry => {
-            return $(`<a href="${entry.link}" class="thumbnail">${entry.source ? `
-            <i class="fab fa-github"></i>` : `
-            `}
-                        <label>${entry.title}</label>
-                        <img src="${entry.img}" alt="${entry.title}"/>
-                    </a>`)
+            return $(`<a href="${entry.link}" class="thumbnail">`).append(
+                entry.source ? $(`<a href="${entry.source}" class="github-link${entry.lightIcon ? ' light' : ''}">`).append(
+                    $('<i class="fab fa-github"></i>')
+                ) : null,
+                entry.label ? $(`<label>`).text(entry.title) : null,
+                $(`<img src="${entry.img}" alt="${entry.title}">`)
+            )
         })
     )];
+}
 
-    // $(`<div class="portfolio">
-    //     <a href="https://jyouya.github.io/Word-Guess-Game/" class="thumbnail">
-    //         <label>Hangman</label>
-    //         <img src="http://lorempixel.com/400/300/cats/1" alt="" />
-    //     </a>
+function drawContact() {
+    return [
+        $('<h2 class="page-title">Contact Me</h2>'),
+        $('<form>').append(
+            $('<label>').text('Name'),
+            $('<input type="text" name="name" placeholder="Your Name" disabled />'),
+            $('<label>').text('Email'),
+            $('<input type="text" name="email" placeholder="example@gmail.com" disabled />'),
+            $('<label>').text('Message'),
+            $('<textarea type="text" name="message" placeholder="Your message goes here..." rows=12 disabled></textarea>'),
+            $('<input id="submit" type="submit" disabled />')
+        ),
+        $('<h2>').html('This form is under construction, but you can email me at <a href="mailto:willwest8xp@gmail.com">willwest8xp@gmail.com</a>')
+    ]
 
-    //     <a href="https://jyouya.github.io/unit-4-game/" class="thumbnail">
-    //         <label>RPG Game</label>
-    //         <img src="http://lorempixel.com/400/300/cats/6" alt="" />
-    //     </a>
-
-    //     <a href="https://jyouya.github.io/TriviaGame/" class="thumbnail">
-    //         <label>Trivia Game</label>
-    //         <img src="http://lorempixel.com/400/300/cats/3" alt="" />
-    //     </a>
-
-    //     <a href="#" class="thumbnail">
-    //         <label>Rutgers Info Widget</label>
-    //         <img src="http://lorempixel.com/400/300/cats/4" alt="" />
-    //     </a>
-
-    //     <a href="#" class="thumbnail">
-    //         <label>Rock Paper Scissors</label>
-    //         <img src="http://lorempixel.com/400/300/cats/5" alt="" />
-    //     </a>
-
-    // </div>`)];
+    // <h2 class="page-title">Contact</h2>
+    //         <form>
+    //             <label>Name</label>
+    //             <input type="text" name="name" placeholder="Your Name" />
+    //             <label>Email</label>
+    //             <input type="text" name="email" placeholder="example@gmail.com" />
+    //             <label>Message</label>
+    //             <textarea type="text" name="message" placeholder="Your message goes here..." rows=12></textarea>
+    //             <input id=submit type="submit">
+    //         </form>
 }
